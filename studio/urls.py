be80a7aaf14226d865f34e3c4a3c62eedb46fe67
INPATHTAMILAN-M <inbarepute@gmail.com>
2024-12-04 13:45:37 +0530

    updated routers on url

--- studio/urls.py
+++ studio/urls.py
@@ -18,19 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('account.urls')),
-    path('', include('postapp.urls')),
+    path('api/account/', include('account.urls')),
+    path('api/postapp/', include('postapp.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
 ]
-urlpatterns = [
-    path('', include(router.urls)),  # Includes all router-generated URLs
+# urlpatterns = [
+#     path('', include(router.urls)),  # Includes all router-generated URLs
 
-]
+# ]
     
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
